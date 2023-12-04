2fbee445675063dea39bf4c168c7ea5f0c538fe0
Thitiwat Kosolpattanadurong <85678732+CHLORG1@users.noreply.github.com>
2023-12-04 23:35:29 +0700

    Add files via upload

--- Integration/statistics.js
+++ Integration/statistics.js
@@ -22,3 +22,26 @@ const boxedContent = match ? match[1] : null;
 console.log(boxedContent);
 
 });
+document.getElementById ("auto_fill").addEventListener ("click", function handleClick(event){
+    const inner_fill = document.getElementsByClassName("integral_display")[0].innerHTML;
+    const match = inner_fill.match(/\\displaystyle([^<]+)<\/script><\/p><\/div>/);
+    console.log(inner_fill);
+    // Check if a match is found
+    if (match && match.length > 1) {
+    var expression = match[1].trim();
+    }
+    document.getElementById("input_answer").value = "\\begin{align}"+'\n'+"\tI &="+ expression+"  \\\\\n  \t&= \\boxed{...} \\\\"+'\n'+"\\end{align}";
+
+});
+var inputField = document.getElementById('input_answer'); // get textarea object
+inputField.onkeydown = function(e) { // list for when any key is pressed
+if (e.keyCode === 9) { // block to catch when tab key is pressed
+    this.setRangeText(
+            '\t',
+            this.selectionStart,
+            this.selectionStart,
+            'end'
+        )
+    return false; //prevent default action
+}
+};
